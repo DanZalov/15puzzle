702b427dfa5d302edfdc0bb7e941053530f4d884
Danil <105085374+DanZalov@users.noreply.github.com>
2022-12-26 21:22:57 +0400

    swipe for phones

--- site.js
+++ site.js
@@ -5,10 +5,13 @@ import {
   randomInit,
 } from './functions.js'
 import { classInit, playTableDiv } from './pagecontent.js'
-import './style.css'
+import './styles/style.css'
 
 export const site = {
   blankIndex: 16,
+  touchStartX: 0,
+  touchStartY: 0,
+  touchedDiv: 0,
   render() {
     playTableDiv()
     randomInit()
@@ -28,21 +31,55 @@ export const site = {
   },
 
   itemSwapHandler(blankIndex) {
-    let $allDivs = document.querySelectorAll('.number-item')
-    for (let i = 0; i < 16; i++) {
-      $allDivs[i].onclick = () => {
-        if (getNeighborIndex(blankIndex).includes(+$allDivs[i].id)) {
+    const $allDivs = document.querySelectorAll('.number-item')
+    if (window.screen.availWidth > 768) {
+      for (let i = 0; i < 16; i++) {
+        $allDivs[i].onclick = () => {
+          if (getNeighborIndex(blankIndex).includes(+$allDivs[i].id)) {
+            document.getElementById(blankIndex).textContent =
+              $allDivs[i].textContent
+            $allDivs[i].textContent = ''
+            document.getElementById(blankIndex).classList.remove('blank')
+            blankIndex = +$allDivs[i].id
+            document.getElementById(blankIndex).classList.add('blank')
+            site.blankIndex = blankIndex
+          }
+          site.activeClassChangeHandler(blankIndex)
+          checkVictory()
+        }
+      }
+    } else {
+      for (let i = 0; i < 16; i++) {
+        $allDivs[i].addEventListener('touchstart', (event) => {
+          this.touchStartX = event.changedTouches[0].screenX
+          this.touchStartY = event.changedTouches[0].screenY
+          this.touchedDiv = +$allDivs[i].id
+        })
+      }
+      const $playTable = document.querySelector('.play-table')
+      $playTable.addEventListener('touchend', (event) => {
+        const touchEndX = event.changedTouches[0].screenX
+        const touchEndY = event.changedTouches[0].screenY
+        if (
+          (touchEndX > this.touchStartX &&
+            this.touchedDiv === blankIndex - 1) ||
+          (touchEndX < this.touchStartX &&
+            this.touchedDiv === blankIndex + 1) ||
+          (touchEndY > this.touchStartY &&
+            this.touchedDiv === blankIndex - 4) ||
+          (touchEndY < this.touchStartY && this.touchedDiv === blankIndex + 4)
+        ) {
           document.getElementById(blankIndex).textContent =
-            $allDivs[i].textContent
-          $allDivs[i].textContent = ''
+            $allDivs[this.touchedDiv - 1].textContent
+          $allDivs[this.touchedDiv - 1].textContent = ''
           document.getElementById(blankIndex).classList.remove('blank')
-          blankIndex = +$allDivs[i].id
+          blankIndex = +$allDivs[this.touchedDiv - 1].id
           document.getElementById(blankIndex).classList.add('blank')
           site.blankIndex = blankIndex
         }
         site.activeClassChangeHandler(blankIndex)
         checkVictory()
-      }
+      })
     }
   },
 
